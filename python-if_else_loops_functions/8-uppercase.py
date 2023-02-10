1daf513f07278a6addeac31efd42532218381d13
Nyiriek <n.peat@alustudent.com>
2023-02-10 06:53:38 -0800

    8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    fir i in range(len(str)):
+    for i in range(len(str)):
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
             number = 32
         else:

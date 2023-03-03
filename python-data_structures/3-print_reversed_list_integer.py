d4ddef2fab551c5e06ae6591740afb451a591fa6
Nyiriek <n.peat@alustudent.com>
2023-03-03 02:07:26 -0800

    3-print_reversed_list_integer.py

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-
     if not my_list:
-        pass
-    else:
-        my_list.reverse()
-        for i range(len(my_list)):
-            print("{:d}".format(my_list[i]))
+        return None
+        for i reversed(my_list):
+            print("{:d}".format[i])

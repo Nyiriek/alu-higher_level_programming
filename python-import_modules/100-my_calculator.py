7b6349ffd6b4cdb7e43277eb6e8a61bff28d7324
Nyiriek <n.peat@alustudent.com>
2023-03-06 02:46:44 -0800

    100-my_calculator.py

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
-    from calculator_1_import add, sub, mul,div
+    """Handle basic arithmetic operations."""
+    from calculator_1 import add, sub, mul,div
     import sys
 
     if len(sys.argv) - 1 != 3:

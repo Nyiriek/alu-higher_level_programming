acad79569be92d5ebc96263722280ceede613138
Nyiriek <n.peat@alustudent.com>
2023-03-06 01:12:34 -0800

    12-roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not insistance(roman_string, str) or roman_string is None:
+    if not isinstance(roman_string, str) or roman_string is None:
         return None
 
     roman_dict = {

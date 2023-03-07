b6c5df6754042341dea08bf86d4882cc536609a0
Nyiriek <n.peat@alustudent.com>
2023-03-07 00:36:39 -0800

    0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     no = 0
-    for i in range(x):
+    for in range(x):
         try:
             print(my_list[i], end='')
             no += 1

04391c9fa357f61b0ec9d82ac9cf2ea2019cb653
Nyiriek <n.peat@alustudent.com>
2023-03-16 15:05:11 +0200

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -2,8 +2,6 @@
 # This code writes a class Square that defines a square based on the initial
 
 """Class Square that defines a square object."""
-
-
 class Square:
     """Present a square."""
     def __init__(self, size):

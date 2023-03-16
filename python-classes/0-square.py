0dffecfc15f7628dea779061eba32988f6dd1a6c
Nyiriek <n.peat@alustudent.com>
2023-03-16 04:59:59 -0700

    0-square.py

---
+++ python-classes/0-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#The following code writes an empty class Square that defines a square
+
+'''
+Define a class square.
+'''
+
+
+class Square:
+    '''
+    Represent a square.
+    '''
+    pass

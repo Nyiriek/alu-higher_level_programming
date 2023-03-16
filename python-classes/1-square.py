fa1a3f8da991a92b46d2c0131c9435d5980202cd
Nyiriek <n.peat@alustudent.com>
2023-03-16 14:39:20 +0200

    1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+# This code writes a class Square that defines a square based on the initial square
 
+"""Class Square that defines a square object.
+"""
+
+class Square:
+    """Present a square."""
+    def __init__(self, size):
+        """Initialize the square.
+
+        Args:
+        size(int): Size of new square.
+        """
+        self.__size = size
+    

4f2b8d45d8f1f39a2dd8d8f2b68b1ce32f30694a
Nyiriek <n.peat@alustudent.com>
2023-03-17 06:37:57 -0700

    6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -54,6 +54,6 @@ class Square:
 
         [print("") for i in range(0, self.__position[1])]
         for i in range (0, self.__size):
-           [print(" ", end="") for j in range(0, sself.__position[0])]
+           [print(" ", end="") for j in range(0, self.__position[0])]
            [print("#", end="") for k in range(0, self.__size)]
            print("")

4663f60f0d63ed46234889b7746af135126fd2ba
Nyiriek <n.peat@alustudent.com>
2023-03-17 03:26:45 -0700

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -11,6 +11,10 @@ class Square:
         Args:
             size (int): Size of new square.
         """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     @property

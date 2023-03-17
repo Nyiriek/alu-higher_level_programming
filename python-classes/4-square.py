b29da6099f6c6661221f7e8a53315481584fe927
Nyiriek <n.peat@alustudent.com>
2023-03-17 03:02:02 -0700

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -14,7 +14,7 @@ class Square:
 
     @property
     def size(self):
-        """Retrieve the size of the square."""
+        """Get the current size of the square."""
         return (self.__size)
 
     @size.setter

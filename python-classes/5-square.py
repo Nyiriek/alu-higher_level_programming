363cb134d5cc0b3deb3085da46745b36c26b91e0
Nyiriek <n.peat@alustudent.com>
2023-03-17 04:12:20 -0700

    5-square.py

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -41,6 +41,6 @@ class Square:
                 print()
             else:
                 for i in range(self.__size):
-                    for n in range(self.__size):
+                    for j in range(self.__size):
                         print("#", end='')
                     print()

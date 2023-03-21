12b48f02ac32bab4d549cdf88139bec2217d7051
Nyiriek <n.peat@alustudent.com>
2023-03-21 16:46:51 +0200

    100-magic_string.py

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(n): 
-    for i in range(1, n+1):
-        return ''.join("Bestchool" * i)
+def magic_string(n):
+    1 += ["BestSchool"]
+    return ", ".join(1)

796c41313d7b06692c01cef8f275f067d2a672a3
Nyiriek <n.peat@alustudent.com>
2023-03-22 15:31:48 +0200

    101-locked_class.py

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -10,5 +10,5 @@ class LockedClass:
 
     def __setattr__(self, name, value):
         if name != 'first_name':
-            raise AttributeError(f"'LockedClass' object has no attribute 'name'")
+            raise AttributeError("'LockedClass' object has no attribute '{__name__}'")
         self.__dict__[name] = value

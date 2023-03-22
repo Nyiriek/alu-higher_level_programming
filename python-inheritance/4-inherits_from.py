ccb6418f3019220757d2f1c809a40872b575d733
Nyiriek <n.peat@alustudent.com>
2023-03-23 00:52:05 +0200

    4-inherits_from.py

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -6,7 +6,7 @@
 def inherits_from(obj, a_class):
     """Returns True/False."""
 
-    if not isinstance(obj, a_class):
-        return True
-    else:
+    if type(obj) is a_class or not isinstance(obj, a_class):
         return False
+    else:
+        return True

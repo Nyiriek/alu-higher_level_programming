76d92aeb993d39b33eeeaf7396ee238fa0d3415f
Nyiriek <n.peat@alustudent.com>
2023-03-23 20:48:26 +0200

    101-add_attribute.py

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -5,6 +5,8 @@
 def add_attribute(a_class, name, value):
     """Adds new attribute to an object."""
 
+    cannot_add = {int, str, float, list, dict tuple, frozenset,type,object}
+
     if type(a_class) in cannot_add:
         raise TypeError("can't add new attribute")
     a_class.__setattr__(name, value)

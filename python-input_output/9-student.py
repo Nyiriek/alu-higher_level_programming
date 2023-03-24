edfd5e0b3c4671e9e8adf26fb26510832bb7bdcb
Nyiriek <n.peat@alustudent.com>
2023-03-24 14:45:54 +0200

    9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -3,13 +3,13 @@
 
 
 class Student:
-    """Contains student data."""
+    """Presents student data."""
     def __init__(self, first_name, last_name, age):
         """Initialize new student."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
-    def t0_json(self):
+    def to_json(self):
         """Returns dict. description of student data."""
         return (self.__dict__)

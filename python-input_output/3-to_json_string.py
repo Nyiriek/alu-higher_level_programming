c59cc4263e5497f4b1351e745741de22de9cfa7b
Nyiriek <n.peat@alustudent.com>
2023-03-24 13:19:50 +0200

    3-to_json_string.py

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Function that returns JSON representation of an object."""
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of my_obj."""
+
+    return json.dumps(my_obj)

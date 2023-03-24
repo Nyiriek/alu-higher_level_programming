d7593cdcde5503ccdafc49500bb48c3cf8dcff51
Nyiriek <n.peat@alustudent.com>
2023-03-24 14:42:14 +0200

    9-student.py

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -4,7 +4,7 @@
 
 class Student:
     """Contains student data."""
-    def __init__(sefl, first_name, last_name, age):
+    def __init__(self, first_name, last_name, age):
         """Initialize new student."""
         self.first_name = first_name
         self.last_name = last_name

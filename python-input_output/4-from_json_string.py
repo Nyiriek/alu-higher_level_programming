f7b577f3d3f883c0d497520c25f9520f32316671
Nyiriek <n.peat@alustudent.com>
2023-03-24 13:32:54 +0200

    4-from_json_string.py

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Function that returns an object represented by JSON string."""
+import json
+
+def from_json_string(my_str):
+    """Returns an objected by a JSON string."""
+    return json.loads(my_str)

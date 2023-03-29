a220d09900d4388ec70f8337a779d202ad0f7346
Nyiriek <n.peat@alustudent.com>
2023-03-29 13:47:00 -0700

    1-create_database_if_missing.sql

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,2 @@
--- Creates a new database
--- Query to create the database htbn_0c_0 in MYSQL server
-CREATE DATABASE IF NOT EXISTS 'htbn_0c_0';
+-- Creates the database hbtn_0c_0 in MySQL server.
+CREATE DATABASE IF NOT EXISTS `htbn_0c_0`;

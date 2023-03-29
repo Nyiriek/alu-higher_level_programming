47e2bc9a29c9764e254db4a3b9978c5359fc5c69
Nyiriek <n.peat@alustudent.com>
2023-03-29 13:59:57 -0700

    2-remove_database.sql

--- SQL_introduction/2-remove_database.sql
+++ SQL_introduction/2-remove_database.sql
@@ -1,2 +1,3 @@
--- Deletes the database htbn_0c_0 from MySQL server.
-DROP DATABASE IF EXISTS htbn_0c_0;
+-- Deletes a database
+-- Query to delete the database hbtn_0c_0 in MySQL server
+DROP DATABASE IF EXISTS hbtn_0c_0;

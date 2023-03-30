483b8065b1fb3ba1c66f480649a58b2d099a1045
Nyiriek <n.peat@alustudent.com>
2023-03-30 08:21:10 -0700

    100-move_to_utf8.sql

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- Converts the database hbtn_0c_0 to UTF8
-USE hbtn_0c_0;
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE second_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0
+ALTER TABLE second_table
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;

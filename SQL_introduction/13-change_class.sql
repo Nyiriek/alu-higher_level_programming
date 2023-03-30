ec30c39f0b45c442bfc5cf8b4eae50c788ef961c
Nyiriek <n.peat@alustudent.com>
2023-03-30 07:34:24 -0700

    13-change_class.sql

--- SQL_introduction/13-change_class.sql
+++ SQL_introduction/13-change_class.sql
@@ -1,2 +1,2 @@
--- Removes all records with score <= 5 in the table second_table in MySQL server
+-- Removes all records with a score <= 5 in the table second_table in the MySQL server
 DROP FROM second_table WHERE score <= 5;

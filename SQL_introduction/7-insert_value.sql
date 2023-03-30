0900dd4790432c20e7141ce5b1bad7299a9b78cc
Nyiriek <n.peat@alustudent.com>
2023-03-30 06:39:22 -0700

    7-insert_value.sql

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
 -- Query that inserts a new row in the table first_table in MySQL server
-INSERT INTO first_table (id, name) VALUES (89," Best School");
+INSERT INTO first_table (id, name) VALUES (89, "Best School");

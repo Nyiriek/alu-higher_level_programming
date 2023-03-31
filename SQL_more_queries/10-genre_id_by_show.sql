bf1b5a5e5687c88e10695c1481d2c7cf48a23181
Nyiriek <n.peat@alustudent.com>
2023-03-30 20:12:16 -0700

    10-genre_id_by_show.sql

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,2 @@
 -- Lists all shows contained in the database hbtn_0d_tvshows that have atleast one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_genres.genre_id;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title, tv_show_genres.genre_id;

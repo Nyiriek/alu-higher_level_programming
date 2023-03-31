de521dd4486a1706fde0bd59de1de0420f5bfc9c
Nyiriek <n.peat@alustudent.com>
2023-03-30 19:55:07 -0700

    9-cities_by_state_join.sql

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Lists all the cities contained in the database
-SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.sate_id IN states.id;
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id;

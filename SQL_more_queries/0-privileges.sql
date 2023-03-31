625e012bef8b8293dd47043770eb7f57826dc595
Nyiriek <n.peat@alustudent.com>
2023-03-30 18:04:17 -0700

    0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Lists all privileges for users user_0d_1 and user_0d_2 in MySQL server
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';

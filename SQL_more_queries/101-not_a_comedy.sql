bada786af29f6016ca8f83d4127ddafdb4aa7f5c
Nyiriek <n.peat@alustudent.com>
2023-03-31 07:10:44 -0700

    101-not_a_comedy.sql

--- SQL_more_queries/101-not_a_comedy.sql
+++ SQL_more_queries/101-not_a_comedy.sql
@@ -1,2 +1,2 @@
 -- Lists all shows with the genre comedy in the database
-SELECT title FROM tv_shows LEFT JOIN (SELECT title FROM tv_shows JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_genres.name = "Comedy" ORDER BY tv_shows.id) comedy_shows ON comedy_shows.title = tv_shows.title WHERE comedy_shows.title IS NULL ORDER BY title;
+SELECT tv_shows.title FROM tv_shows LEFT JOIN (SELECT tv_shows.title FROM tv_shows JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id WHERE tv_genres.name = "Comedy" ORDER BY tv_shows.id) comedy_shows ON comedy_shows.title = tv_shows.title WHERE comedy_shows.title IS NULL ORDER BY tv_shows.title;

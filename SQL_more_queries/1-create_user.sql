0d169c3c9a7c31f05a4a0709c22861b61d7e9f71
Nyiriek <n.peat@alustudent.com>
2023-03-31 04:05:23 -0700

    1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Creates user with all privileges in the MySQL server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+

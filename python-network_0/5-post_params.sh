df47118a055b60a3dc90946a066a638e1d3992a2
Nyiriek <n.peat@alustudent.com>
2023-04-04 17:05:53 -0700

    5-post_params.sh

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that sends a POST request to the passed URL and displays the body of the response
-curl -sXd POST "email=test@gmail.com&subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$1"

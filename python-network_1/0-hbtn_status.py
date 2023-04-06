ced9f525a5bf6ed3d102f3ccc6ba5406c3a3fe0c
Nyiriek <n.peat@alustudent.com>
2023-04-06 23:01:06 +0000

    0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-"""A script that fetches "https://alu-intranet.hbtn.io/status"""
+"""A script that
+ fetches "https://alu-intranet.hbtn.io/status.
+"""
 import urllib.request
 
 

8b3693a8984f4bcbd1b5597929ef8d69419d95da
Nyiriek <n.peat@alustudent.com>
2023-04-07 19:14:37 +0000

    2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """Sends a POST request to a given URL with a given email.
 Usage: ./2-post_email.py <URL> <email>
   - Displays the body of the response.
 """
 import argv from sys
 from urllib import request, parse
-
 
 
 if __name__ == "__main__":

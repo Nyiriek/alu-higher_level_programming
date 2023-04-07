503f0fdf00ff1e368a688ee3f1d1da1dee64708e
Nyiriek <n.peat@alustudent.com>
2023-04-07 19:30:18 +0000

    2-post_email.py

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""Sends a POST request to a given URL with a given email.
-Usage: ./2-post_email.py <URL> <email>
-  - Displays the body of the response.
+"""  script that takes in a URL and an email, sends a POST request to the
+    passed URL with the email as a parameter, and displays the body of the
+    response (decoded in utf-8)
 """
-from sys import argv
-import urllib.reques
+import urllib.request
 import urllib.parse
+from sys import argv
 
 
 def post_email():
-    """Sends a POST request to the passed URL with the email as a parameter."""
-    url = sys.argv[1]
-    values = {"email": sys.argv[2]}
+    """sends a POST request to the passed URL with the email as a parameter"""
+    url = argv[1]
+    values = {'email': argv[2]}
     data = urllib.parse.urlencode(values)
-    data = data.encode("utf-8") # The data should be in bytes
-
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as response:
-        print(response.read().decode("utf-8"))
+    data = data.encode('utf-8')  # data should be bytes
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read().decode('utf-8')
+    print(the_page)
 
 
 if __name__ == "__main__":

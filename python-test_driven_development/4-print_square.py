84b6f7c1eaa8492a49b86d093949c739595d4669
Nyiriek <n.peat@alustudent.com>
2023-04-25 08:53:30 -0700

    Print square

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Function that prints a square with a # characater"""
+
+
+def print_square(size):
+    """Presents a square with a # character"""
+    if not isinstance(size, int):
+        raise TypeError("Size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        print("#" * size)

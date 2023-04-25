7dbb9816344203f7abeb5b05c3f1f55b109c3756
Nyiriek <n.peat@alustudent.com>
2023-04-25 08:00:31 -0700

    Say my name

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""A function that prints say my name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Prints my name
+    Args:
+        first_name: First name
+        last_name: Last name
+
+    Raises:
+        TypeError: if first_name or last_name is not a string
+    """
+
+    if type(first_name) != str:
+        raise TypeError("firts_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))

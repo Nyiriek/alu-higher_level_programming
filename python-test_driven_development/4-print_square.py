05ffd57d684441c1dc7c118ea7c603283369ed61
Nyiriek <n.peat@alustudent.com>
2023-04-25 19:53:51 -0700

    Print square

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -10,5 +10,6 @@ def print_square(size):
     if size < 0:
         raise ValueError("size must be >= 0")
 
-    for _ in range(size):
-        print("#" * size)
+    for i in range(size):
+        [print("#", end="") for k in range(size)]
+        print("")

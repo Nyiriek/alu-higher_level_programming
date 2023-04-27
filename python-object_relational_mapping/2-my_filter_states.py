28d1e8da0694d49f112e05ffce3a3a6268921c4f
Nyiriek <n.peat@alustudent.com>
2023-04-27 16:30:42 -0700

    Filter states by user input

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""A script that takes in an argument and displays all its values in the database"""
+"""A script that takes in an argument and displays\
+        all its values in the database"""
 import MySQLdb
 import sys
 
@@ -7,15 +8,15 @@ import sys
 def select_states():
     """Displays all the values of an argument in a database"""
 
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2],
-                         port=3306, db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], port=3306, db=sys.argv[3])
 
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE\
             name LIKE BINARY '{:s}'\
             ORDER BY id".format(sys.argv[4]))
 
-    states = cursor.fetchall()
+    states = cur.fetchall()
     for states in states:
         print(states)
 

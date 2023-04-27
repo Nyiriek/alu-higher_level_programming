423a5fa51313c1ba241eeea571d7faed074e663b
Nyiriek <n.peat@alustudent.com>
2023-04-27 15:40:46 -0700

    Filter states starting with 'N'

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""A script that lists all states with names starting with N"""
+import MySQLdb
+import sys
+
+
+def select_states():
+    """Lists all states with N"""
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+    cur.execute("SELECT FROM states WHERE name LIKE 'N%' ORDER BY id")
+
+    states = cur.fetchall()
+    for states in states:
+        if states[1][2] == 'N':
+        print(states)
+
+    cur.close()
+    db.close()
+
+
+    if __name__ == "__main__":
+        select_states()

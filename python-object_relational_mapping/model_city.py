8f34ddfddd75fb037bc4489d3318ee9d6e8557fa
Nyiriek <n.peat@alustudent.com>
2023-04-28 06:26:27 -0700

    Cities in state

---
+++ python-object_relational_mapping/model_city.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Contains the class definition of a city"""
+
+from sqlalchemy import Column, Integer, String, ForeignKey
+from model_state import Base
+
+
+class City(Base):
+    """Class City"""
+    __tablename__ = 'cities'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))

f688c3cdb825d4fc9780fe9c19a71524e8cac65d
Nyiriek <n.peat@alustudent.com>
2023-04-28 03:23:14 -0700

    All cities by state

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -5,20 +5,24 @@ import sys
 
 
 def my_safe_filter_states():
-    """Takes in the name of the state as an\
-            argument and lists all cities"""
+    """Takes in the name of the state and lists\
+            all cities in the database"""
 
     db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], port=3306, db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT cities.name FROM cities\
+    cur.execute( "SELECT cities.name FROM cities\
             JOIN states ON cities.state_id = states.id\
-            WHERE states.name LIKE %s ORDER BY cities.id", (sys.argv[4]))
+            AND states.name = %s\
+            ORDER BY cities.id ASC", sys.argv[4],)
 
     cities = cur.fetchall()
+    cites = []
+
     for cities in cities:
-        cities.append(cities[0])
-    print(", ".join(cities))
+        cites.append(cities[0])
+
+    print(", ".join(cites))
     cur.close()
     db.close()
 

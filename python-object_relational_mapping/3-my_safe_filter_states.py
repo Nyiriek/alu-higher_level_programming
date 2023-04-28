8a984f722d2fae23aaf895ccebed5de4c1a674dc
Nyiriek <n.peat@alustudent.com>
2023-04-27 17:03:53 -0700

    SQL injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A script that takes in arguments and displays\
+        its values in the database"""
+
+import MySQLdb
+import sys
+
+
+def select_states():
+    """Displays the values of arguments in the database"""
+
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1], 
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE\
+            BINARY %s ORDER BY id", (argv[4]))
+
+    states = cur.fetchall()
+    for states in states:
+        prints(states)
+
+    cur.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    select_states()

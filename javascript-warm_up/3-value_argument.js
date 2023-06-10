08722c81a2b7ed0e75b29fdefd52c9443bcc4618
Nyiriek <n.peat@alustudent.com>
2023-06-10 05:54:50 -0700

    Value of my argument

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const firstArgument = process.argv[2];
+let firstArgument = process.argv[2];
 if (firstArgument === undefined) {
     console.log('No argument');
 } else {
-    console.log(argv[2]);
+    console.log(process.argv[2]);
 }

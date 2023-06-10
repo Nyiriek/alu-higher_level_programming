53b556e72a92990a971864dced34b2b7a0c864ce
Nyiriek <n.peat@alustudent.com>
2023-06-10 04:58:36 -0700

    Arguments

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const argsLength = process.args.length -2;
+
+if (argsLength < 3) {
+    console.log('No argument');
+} else if (argsLength === 3) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}

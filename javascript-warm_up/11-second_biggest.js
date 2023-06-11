7a0590279f1d15a1019e738d9fd3db2f0bbc5421
Nyiriek <n.peat@alustudent.com>
2023-06-11 07:17:08 -0700

    Add second biggest

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 const args = process.argv.slice(2);
 const integers = args.map(arg => parseInt(arg));
 
-if (integers.length === 1) {
+if (integers.length < 2) {
   console.log(0);
 } else {
   const sortedIntegers = integers.sort((a, b) => b - a);

8569bcb85af836c5ef17944babeb1b071eebd8ec
Nyiriek <n.peat@alustudent.com>
2023-06-11 04:43:38 -0700

    Add a script that prints a square

---
+++ javascript-warm_up/8-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const size = parseInt(process.argv[2]);
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let m = 0; m < size; m++) {
+    let line = '';
+    for (let k = 0; k < size; k++) {
+      line = 'X';
+    }
+    console.log(line);
+  }
+}

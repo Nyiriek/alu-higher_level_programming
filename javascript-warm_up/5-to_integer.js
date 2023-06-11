b22aa254b175639b19ee65c69aa9cc58b0a4e4b9
Nyiriek <n.peat@alustudent.com>
2023-06-11 03:34:35 -0700

    Add an integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const num = parseInt(process.argv[2]);
-if (isNaN(num)) {
+const number = parseInt(process.argv[2]);
+if (isNaN(number)) {
 	console.log('Not a number');
 } else {
-	console.log('My number:' + numb);
+	console.log('My number:', number);
 }

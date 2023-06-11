36da97548ea4a0eab19f109922b888b6fe27cd47
Nyiriek <n.peat@alustudent.com>
2023-06-11 04:01:52 -0700

    Add loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const myStatements = ['C is fun', 'Python is cool', 'JavaScript i amazing'];
+const myStatements = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < 3; i++) {
   console.log(myStatements[i]);
 }

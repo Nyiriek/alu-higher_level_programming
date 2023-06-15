6342eefba8e01c29dab550774583b972874153ad
Nyiriek <n.peat@alustudent.com>
2023-06-15 04:01:59 -0700

    add readme

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
+
+fs.readFile(filePath, 'utf-8', (error, content) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(content);
+  }
+});
